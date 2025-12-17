af2946381d52cfd8152f7de85ec7da11c44de08f
bala-amypo <bala@amypo.in>
2025-12-17 05:30:54 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -4,4 +4,6 @@ import com.example.demo.Repository.UserAccountRepository;
 import com.example.demo.service.userAccountService;
 import org.springframework.security.crypto.passwordEncoder;
 import org.springframework.stereotype.Service;
-import org.
+import java.util.List;
+
+@service
