c72fc36e393b835c8e40632799a2851f4203ca64
bala-amypo <bala@amypo.in>
2025-12-17 05:44:48 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -4,6 +4,14 @@ import com.example.demo.Repository.UserAccountRepository;
 import com.example.demo.service.userAccountService;
 import org.springframework.security.crypto.password.passwordEncoder;
 import org.springframework.stereotype.Service; //steereotype->automatic bean creation, define business logic, dependency injection
-import java.util.List;
+import java.util.List; //allows to use List interface
 
 @service
+public class UserAccountServiceImpl implements UserAccountService{
+    private final UserAccountRepository userAccountRepository;
+    private final PasswordEncoder passwordEncoder;
+    public UserAccountServiceImpl(UserAccountRepository userAccountRepository, PasswordEncoder passwordEncoder){
+        this.userAccountRepository=userAccount
+    }
+    }
+}
