a32989f8b1d76edce6bd35bed725c97bb3818f57
bala-amypo <bala@amypo.in>
2025-12-17 05:39:48 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -3,7 +3,7 @@ import com.example.demo.entity.UserAccount;
 import com.example.demo.Repository.UserAccountRepository;
 import com.example.demo.service.userAccountService;
 import org.springframework.security.crypto.password.passwordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service; //steereotype->automatic bean creation, define business logic, dependency injection
 import java.util.List;
 
 @service
